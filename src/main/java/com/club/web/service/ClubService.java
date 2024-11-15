0929f83ab5275e676d280260cfb349a89fe720e1
zifrz <114908186+zifrz@users.noreply.github.com>
2024-11-15 23:56:05 +0530

    feat: added create club functionality

--- src/main/java/com/club/web/service/ClubService.java
+++ src/main/java/com/club/web/service/ClubService.java
@@ -1,9 +1,12 @@
 package com.club.web.service;
 
 import com.club.web.dto.ClubDto;
+import com.club.web.models.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
+
+    Club saveClub(Club club);
 }
