aa668d526a9502cdfa5f3c280f3014ddfa8b4147
zifrz <114908186+zifrz@users.noreply.github.com>
2024-11-18 23:22:17 +0530

    refactor: made events a list

--- src/main/java/com/club/web/models/Club.java
+++ src/main/java/com/club/web/models/Club.java
@@ -9,8 +9,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Data
@@ -35,5 +35,5 @@ public class Club {
     private LocalDateTime updatedOn;
 
     @OneToMany(mappedBy = "club", cascade = CascadeType.REMOVE)
-    private Set<Event> events = new HashSet<>();
+    private List<Event> events = new ArrayList<>();
 }
