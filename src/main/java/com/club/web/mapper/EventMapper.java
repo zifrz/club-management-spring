347baa12c65eb14d075e9025d6918ec42df4199d
zifrz <114908186+zifrz@users.noreply.github.com>
2024-11-18 23:31:32 +0530

    add: Event Mapper to EventDto

--- src/main/java/com/club/web/mapper/EventMapper.java
+++ src/main/java/com/club/web/mapper/EventMapper.java
@@ -6,7 +6,8 @@ import com.club.web.models.Event;
 public class EventMapper {
     public static Event mapToEvent(EventDto eventDto) {
         return Event
-                .builder().id(eventDto.getId())
+                .builder()
+                .id(eventDto.getId())
                 .name(eventDto.getName())
                 .startTime(eventDto.getStartTime())
                 .endTime(eventDto.getEndTime())
@@ -14,6 +15,19 @@ public class EventMapper {
                 .photoUrl(eventDto.getPhotoUrl())
                 .createdOn(eventDto.getCreatedOn())
                 .updatedOn(eventDto.getUpdatedOn())
+                .build();
+    }
+
+    public static EventDto mapToEventDto(Event event) {
+        return EventDto.builder()
+                .id(event.getId())
+                .name(event.getName())
+                .startTime(event.getStartTime())
+                .endTime(event.getEndTime())
+                .type(event.getType())
+                .photoUrl(event.getPhotoUrl())
+                .createdOn(event.getCreatedOn())
+                .updatedOn(event.getUpdatedOn())
                 .build();
     }
 }
