45993aff464d8b1e1fbe16199eff174eefa29a3d
zifrz <114908186+zifrz@users.noreply.github.com>
2024-11-18 23:24:10 +0530

    add: events to ClubDto

--- src/main/java/com/club/web/dto/ClubDto.java
+++ src/main/java/com/club/web/dto/ClubDto.java
@@ -1,21 +1,25 @@
 package com.club.web.dto;
 
+import com.club.web.models.Event;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Builder
 public class ClubDto {
     private Long id;
-    @NotEmpty (message = "Club title should not be empty")
+    @NotEmpty(message = "Club title should not be empty")
     private String title;
-    @NotEmpty (message = "Photo Link should not be empty")
+    @NotEmpty(message = "Photo Link should not be empty")
     private String photoUrl;
-    @NotEmpty (message = "Content should not be empty")
+    @NotEmpty(message = "Content should not be empty")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
+    private List<EventDto> events;
 }
