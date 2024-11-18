d24877ab97820f48f8da100f73dc406788a73787
zifrz <114908186+zifrz@users.noreply.github.com>
2024-11-19 01:47:11 +0530

    feat: Events Edit

--- src/main/java/com/club/web/controller/EventController.java
+++ src/main/java/com/club/web/controller/EventController.java
@@ -3,9 +3,11 @@ package com.club.web.controller;
 import com.club.web.dto.EventDto;
 import com.club.web.models.Event;
 import com.club.web.service.EventService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,9 +46,31 @@ public class EventController {
         return "events-create";
     }
 
+    @GetMapping("/events/{eventId}/edit")
+    public String editEventForm(@PathVariable("eventId") Long eventId, Model model) {
+        EventDto eventDto = eventService.findByEventId(eventId);
+        model.addAttribute("event", eventDto);
+        return "events-edit";
+    }
+
     @PostMapping("/events/{clubId}")
     public String createEvent(@PathVariable("clubId") Long clubId, @ModelAttribute EventDto eventDto, Model model) {
         eventService.createEvent(clubId, eventDto);
         return "redirect:/clubs/" + clubId;
+    }
+
+    @PostMapping("/events/{eventId}/edit")
+    public String updateEvent(@PathVariable("eventId") Long eventId,
+                              @Valid @ModelAttribute("event") EventDto event,
+                              BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            model.addAttribute("event", event);
+            return "events-edit";
+        }
+        EventDto eventDto = eventService.findByEventId(eventId);
+        event.setId(eventId);
+        event.setClub(eventDto.getClub());
+        eventService.updateEvent(event);
+        return "redirect:/events";
     }
 }
