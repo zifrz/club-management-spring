dea39f94b1cbab91ffc45b9e552731f5fc3e9ed5
zifrz <114908186+zifrz@users.noreply.github.com>
2024-11-19 02:13:14 +0530

    chore: reformatting

--- src/main/java/com/club/web/models/UserEntity.java
+++ src/main/java/com/club/web/models/UserEntity.java
@@ -22,6 +22,17 @@ public class UserEntity {
     private String email;
     private String password;
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "users_roles", joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")}, inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = {
+                    @JoinColumn(
+                            name = "user_id",
+                            referencedColumnName = "id")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(
+                            name = "role_id",
+                            referencedColumnName = "id")
+            })
     private List<Role> roles = new ArrayList<>();
 }
