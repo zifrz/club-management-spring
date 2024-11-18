8c680aa02f49534bb39cc0f6158bc252809f9877
zifrz <114908186+zifrz@users.noreply.github.com>
2024-11-18 22:02:36 +0530

    fix: typo in EventRepository

--- src/main/java/com/club/web/repository/EventRepository.java
+++ src/main/java/com/club/web/repository/EventRepository.java
@@ -3,6 +3,6 @@ package com.club.web.repository;
 import com.club.web.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EvenRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 
 }
