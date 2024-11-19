a935c493192e24d043f5cd4738431c0cb5957a75
zifrz <114908186+zifrz@users.noreply.github.com>
2024-11-19 19:04:11 +0530

    add: SecurityConfig for Login

---
+++ src/main/java/com/club/web/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.club.web.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/register", "/clubs").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .permitAll());
+
+        return http.build();
+    }
+}
